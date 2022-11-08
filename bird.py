77275be4783e4cd8edc38f57f96703baf91bf673
Ethan Cerami <cerami@gmail.com>
2022-11-08 10:13:57 -0500

    Initial Commit

---
+++ bird.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bird(Sprite):
+    """A class to manage the bird."""
+
+    def __init__(self, game):
+        """Initialize the bird and set its starting position."""
+        pass
+
+    def update(self):
+        """Update the bird's position based on movement flags."""
+        pass
+
+    def blitme(self):
+        """Draw the bird at its current location."""
+        pass
---
+++ plant.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Plant(Sprite):
+    """A class to manage the plant."""
+
+    def __init__(self, game):
+        """Initialize the plant and set its starting position."""
+        pass
+
+    def blitme(self):
+        """Draw the plant at its current location."""
+        pass
